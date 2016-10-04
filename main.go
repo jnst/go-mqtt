2e24ad3582ba983347be55780b22e34df167dd10
Jun Sato <jun.sato@translimit.co.jp>
2016-10-04 23:02:32 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"os"
+	"time"
+)
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	mqtt.ERROR = log.New(os.Stdout, "", 0)
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("gotrivial")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	topicName := "go-mqtt/sample"
+	qos := uint8(0)
+	retain := false
+
+	for i := 0; i < 5; i++ {
+		text := fmt.Sprintf("this is msg #%d", i)
+		token := c.Publish(topicName, qos, retain, text)
+		token.Wait()
+	}
+
+	time.Sleep(6 * time.Second)
+
+	if token := c.Unsubscribe(topicName); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
+
+	time.Sleep(1 * time.Second)
+}
